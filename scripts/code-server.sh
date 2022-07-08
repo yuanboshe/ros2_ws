e1aba5b44cd9803bca6f366ca540a6c4e8de0b30
yuanboshe <yuanboshe@yeah.net>
2022-07-08 20:54:28 +0800

    Update code-server.sh

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -7,7 +7,11 @@ WORK_PATH=$(pwd)
 #llvm-vs-code-extensions.vscode-clangd: CPP格式化，提示等功能。首次打开CPP会要求下载后台程序。
 #cheshirekow.cmake-format: CMakeLists.txt格式化
 #mhutchie.git-graph: git graph
-code-server --install-extension llvm-vs-code-extensions.vscode-clangd cheshirekow.cmake-format mhutchie.git-graph
+#foxundermoon.shell-format: format shell
+code-server --install-extension llvm-vs-code-extensions.vscode-clangd \
+    --install-extension cheshirekow.cmake-format \
+    --install-extension mhutchie.git-graph \
+    --install-extension foxundermoon.shell-format
 
 #cheshirekow.cmake-format的依赖
 pip install cmakelang
