d188b7ab659da5d3b9e4cb85d9832b82d03dbc33
yuanboshe <yuanboshe@yeah.net>
2022-07-08 20:44:46 +0800

    Update code-server.sh

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -4,7 +4,10 @@
 cd "$(dirname "$0")" || exit $?
 WORK_PATH=$(pwd)
 
-#vscode-clangd: CPP格式化，提示等功能。首次打开CPP会要求下载后台程序。
-code-server --install-extension llvm-vs-code-extensions.vscode-clangd
+#llvm-vs-code-extensions.vscode-clangd: CPP格式化，提示等功能。首次打开CPP会要求下载后台程序。
+#cheshirekow.cmake-format: CMakeLists.txt格式化
+#mhutchie.git-graph: git graph
+code-server --install-extension llvm-vs-code-extensions.vscode-clangd cheshirekow.cmake-format mhutchie.git-graph
 
-
+#cheshirekow.cmake-format的依赖
+pip install cmakelang
